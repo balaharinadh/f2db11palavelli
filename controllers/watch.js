70074b35cb219c6240e0764b5a7dd9b2b1d63ce9
balaharinadh <111669556+balaharinadh@users.noreply.github.com>
2022-11-08 22:19:20 -0600

    part 7 done

--- controllers/watch.js
+++ controllers/watch.js
@@ -38,3 +38,43 @@ exports.watch_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // Handle Costume create on POST.
+exports.costume_create_post = async function(req, res) {
+console.log(req.body)
+let document = new Costume();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.costume_type = req.body.costume_type;
+document.cost = req.body.cost;
+document.size = req.body.size;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+// Handle watch create on POST.
+exports.watch_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new watch();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"watch_type":"goat", "cost":12, "size":"large"}
+    document.watch_brand = req.body.watch_brand;
+    document.watch_color = req.body.watch_color;
+    document.watch_cost = req.body.watch_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
