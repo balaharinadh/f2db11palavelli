c890dc34f906ee87b9ef8bcf93045592610bd17e
balaharinadh <111669556+balaharinadh@users.noreply.github.com>
2022-11-15 21:25:24 -0600

    part 6 done

--- controllers/watch.js
+++ controllers/watch.js
@@ -114,3 +114,29 @@ exports.watch_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+// Handle building the view for creating a watch.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.watch_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('watchcreate', { title: 'Watch Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a watch.
+// query provides the id
+exports.watch_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await watch.findById(req.query.id)
+    res.render('watchupdate', { title: 'watch Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/watch.js
+++ routes/watch.js
@@ -6,6 +6,10 @@ var router = express.Router();
 router.get('/', watch_controlers.watch_view_all_Page);
 /* GET detail watch page */
 router.get('/detail', watch_controlers.watch_view_one_Page);
+/* GET create watch page */
+router.get('/create', watch_controlers.watch_create_Page);
+/* GET create update page */
+router.get('/update', watch_controlers.watch_update_Page);
 
 module.exports = router;
 
