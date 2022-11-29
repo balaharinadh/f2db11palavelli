b13e3a813bbc9c8d875439aadfdbb1b977f62c41
balaharinadh <111669556+balaharinadh@users.noreply.github.com>
2022-11-29 17:51:59 -0600

    part 4 done

--- routes/index.js
+++ routes/index.js
@@ -46,6 +46,8 @@ router.get('/login', function (req, res) {
   res.render('login', { title: 'Watch App Login', user: req.user });
 });
 router.post('/login', passport.authenticate('local'), function (req, res) {
+  if(req.session.returnTo)
+res.redirect(req.session.returnTo);
   res.redirect('/');
 });
 router.get('/logout', function(req, res, next){// use post or delete for better safety
