2fc6ee43b593b21e29a77bb138c5a3cb324db728
balaharinadh <111669556+balaharinadh@users.noreply.github.com>
2022-11-29 18:35:42 -0600

    bonus part is done

--- models/watch.js
+++ models/watch.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const watchSchema = mongoose.Schema({
 watch_brand: String,
 watch_cost: {type:Number,min:150},
-watch_color: String
+watch_color: {type:String,maxLength:15},
 })
 module.exports = mongoose.model("watch",
 watchSchema)
--- routes/watch.js
+++ routes/watch.js
@@ -15,11 +15,11 @@ router.get('/', watch_controlers.watch_view_all_Page);
 /* GET detail watch page */
 router.get('/detail', watch_controlers.watch_view_one_Page);
 /* GET create watch page */
-router.get('/create', watch_controlers.watch_create_Page);
+router.get('/create',secured, watch_controlers.watch_create_Page);
 /* GET create update page */
 router.get('/update',secured, watch_controlers.watch_update_Page);
 /* GET delete watch page */
-router.get('/delete', watch_controlers.watch_delete_Page);
+router.get('/delete',secured, watch_controlers.watch_delete_Page);
 
 module.exports = router;
 
