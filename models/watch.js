68d6b13ecda01e491669876a43211f102373264c
balaharinadh <111669556+balaharinadh@users.noreply.github.com>
2022-11-29 18:36:46 -0600

    changes done

--- models/watch.js
+++ models/watch.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose")
 const watchSchema = mongoose.Schema({
 watch_brand: String,
 watch_cost: {type:Number,min:150},
-watch_color: {type:String,maxLength:15},
+watch_color: {type:String,maxLength:15}
 })
-module.exports = mongoose.model("watch",
-watchSchema)
+module.exports = mongoose.model("watch",watchSchema)
 
 
